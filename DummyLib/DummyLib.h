b1ed00fa62cdad97175090adef773dbc2737bbda
Morgan Chen <morganchen12@gmail.com>
2016-10-24 13:42:52 -0700

    initial commit

---
+++ DummyLib/DummyLib.h
@@ -0,0 +1,19 @@
+//
+//  DummyLib.h
+//  DummyLib
+//
+//  Created by Morgan Chen on 10/24/16.
+//  Copyright © 2016 Dummy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DummyLib.
+FOUNDATION_EXPORT double DummyLibVersionNumber;
+
+//! Project version string for DummyLib.
+FOUNDATION_EXPORT const unsigned char DummyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyLib/PublicHeader.h>
+
+
